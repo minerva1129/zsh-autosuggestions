9d56e32ac0eb38815045f206a5d98a9f91454f64
minerva <minerva1129@users.noreply.github.com>
2016-07-10 16:55:41 +0900

    Use custom values if exist

--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -32,15 +32,16 @@
 # Color to use when highlighting suggestion
 # Uses format of `region_highlight`
 # More info: http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Widgets
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
+: ${ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE:='fg=8'}
 
 # Prefix to use when saving original versions of bound widgets
-ZSH_AUTOSUGGEST_ORIGINAL_WIDGET_PREFIX=autosuggest-orig-
+: ${ZSH_AUTOSUGGEST_ORIGINAL_WIDGET_PREFIX:=autosuggest-orig-}
 
-ZSH_AUTOSUGGEST_STRATEGY=default
+: ${ZSH_AUTOSUGGEST_STRATEGY:=default}
 
 # Widgets that clear the suggestion
 ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(
+	"${ZSH_AUTOSUGGEST_CLEAR_WIDGETS[@]}"
 	history-search-forward
 	history-search-backward
 	history-beginning-search-forward
@@ -54,6 +55,7 @@ ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(
 
 # Widgets that accept the entire suggestion
 ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=(
+	"${ZSH_AUTOSUGGEST_ACCEPT_WIDGETS[@]}"
 	forward-char
 	end-of-line
 	vi-forward-char
@@ -63,10 +65,12 @@ ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=(
 
 # Widgets that accept the entire suggestion and execute it
 ZSH_AUTOSUGGEST_EXECUTE_WIDGETS=(
+	"${ZSH_AUTOSUGGEST_EXECUTE_WIDGETS[@]}"
 )
 
 # Widgets that accept the suggestion as far as the cursor moves
 ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(
+	"${ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS[@]}"
 	forward-word
 	vi-forward-word
 	vi-forward-word-end
